9e749f0a41ac04d658f1d0307ffba5ef052a9f4d
Odrian OP <boodev22@gmail.com>
2019-07-03 14:08:58 -0500

    very 1st commit

---
+++ _site/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(window).scroll(function() {
+  	if($(document).scrollTop() > 10) {
+    	$('nav').addClass('shrink');
+    }
+    else {
+    $('nav').removeClass('shrink');
+    }
+  });
+
+});
